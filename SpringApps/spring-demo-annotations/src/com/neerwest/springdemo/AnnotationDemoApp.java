136cbb171b4266b928b8aff19ce4138fdfe7cb70
jcromerohdz <jcromerohdz@gmail.com>
2018-07-29 08:58:28 -0700

    Default annotatio name component

--- SpringApps/spring-demo-annotations/src/com/neerwest/springdemo/AnnotationDemoApp.java
+++ SpringApps/spring-demo-annotations/src/com/neerwest/springdemo/AnnotationDemoApp.java
@@ -11,7 +11,7 @@ public class AnnotationDemoApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
--- SpringApps/spring-demo-annotations/src/com/neerwest/springdemo/TennisCoach.java
+++ SpringApps/spring-demo-annotations/src/com/neerwest/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.neerwest.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
